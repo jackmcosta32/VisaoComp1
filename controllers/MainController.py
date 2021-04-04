d07d95dff120fd752ac9e268e509f812c528a85b
Joao Manoel Macedo Moreira da Costa <joao.mm.costa@aluno.ufes.br>
2021-04-04 14:52:03 -0300

    Update: Adicionada movimentação em torno dos eixos do mundo, câmera, e do personagem

--- controllers/MainController.py
+++ controllers/MainController.py
@@ -1,9 +1,10 @@
+from math import pi, cos, sin
+
 import numpy as np
 
 from models.Actor import Actor
 from models.Axis import Axis
 from models.Camera import Camera
-from math import pi, cos, sin
 
 
 class MainController:
@@ -14,48 +15,33 @@ class MainController:
 
     def move_camera(
             self,
-            target_point: np.ndarray = None,
+            target_coordinate: np.ndarray = None,
             rotation_angle: float = None,
-            rotation_axis: str = None
+            rotation_axis: str = None,
+            reference_axis: str = None
     ):
-        if target_point is None:
-            target_point = np.zeros(3)
-
-        reverse_movement_matrix = self.get_reverse_movement_matrix(
-            movement_matrix=self.camera.previous_movement_matrix
+        self.move_object(
+            self.camera,
+            target_coordinate,
+            rotation_angle,
+            rotation_axis,
+            reference_axis
         )
-        self.camera.move(reverse_movement_matrix)
-        movement_matrix = self.get_movement_matrix(
-            current_point=self.camera.coordinate,
-            target_point=target_point,
-            rotation_angle=rotation_angle,
-            rotation_axis=rotation_axis
-        )
-        self.camera.move(movement_matrix)
 
     def move_actor(
             self,
-            target_point: np.ndarray = None,
+            target_coordinate: np.ndarray = None,
             rotation_angle: float = None,
-            rotation_axis: str = None
+            rotation_axis: str = None,
+            reference_axis: str = None
     ):
-        if target_point is None:
-            target_point = np.zeros(3)
-
-        # Reverts the previous movement
-        reverse_movement_matrix = self.get_reverse_movement_matrix(
-            movement_matrix=self.actor.previous_movement_matrix
+        self.move_object(
+            self.actor,
+            target_coordinate,
+            rotation_angle,
+            rotation_axis,
+            reference_axis
         )
-        self.actor.move(reverse_movement_matrix)
-
-        # Makes the new movement
-        movement_matrix = self.get_movement_matrix(
-            current_point=self.actor.coordinate,
-            target_point=target_point,
-            rotation_angle=rotation_angle,
-            rotation_axis=rotation_axis
-        )
-        self.actor.move(movement_matrix)
 
     def draw_camera_view(self):
         print('GET CAMERA VIEW')
@@ -66,10 +52,78 @@ class MainController:
         self.actor.draw(plot_axis)
         self.camera.draw(plot_axis)
 
+    def move_object(
+            self,
+            object: [Actor, Camera],
+            target_coordinate: np.ndarray = None,
+            rotation_angle: float = None,
+            rotation_axis: str = None,
+            reference_axis: str = None
+    ):
+        # Sets the world axis as the default reference axis
+        if reference_axis is None:
+            reference_axis = 'world'
+
+        # Sets the target point to 0, if its not declared
+        if target_coordinate is None:
+            target_coordinate = np.zeros(3)
+
+        # Gets the axis thats the movement is related to
+        if reference_axis == 'actor':
+            axis_coordinate = self.actor.axis.coordinate
+        elif reference_axis == 'camera':
+            axis_coordinate = self.camera.axis.coordinate
+        else:
+            axis_coordinate = np.zeros(3)
+
+        # Gets the reverse of the previous movement matrix
+        reverse_movement_matrix = self.get_reverse_movement_matrix(
+            movement_matrix=object.previous_movement_matrix
+        )
+
+        object.move(reverse_movement_matrix)
+
+        # Get's the next movement matrix
+        if type(object).__name__ == reference_axis.capitalize():
+            # Rotates the object at his own axis than moves it to it's final position
+            movement_matrix = self.get_movement_matrix(
+                current_point=object.axis.coordinate,
+                target_point=target_coordinate,
+                rotation_angle=rotation_angle,
+                rotation_axis=rotation_axis
+            )
+        else:
+            # Moves the object back to the reference axis
+            neg_translation_matrix = self.get_movement_matrix(
+                current_point=object.coordinate,
+                target_point=target_coordinate - axis_coordinate
+            )
+
+            # Rotates the object at the selected axis
+            rotation_matrix = self.get_movement_matrix(
+                rotation_angle=rotation_angle,
+                rotation_axis=rotation_axis
+            )
+
+            # Moves the object to it's final position
+            pos_translation_matrix = self.get_movement_matrix(
+                current_point=np.zeros(3),
+                target_point=axis_coordinate
+            )
+
+            # Gets the new movement matrix
+            movement_matrix = np.linalg.multi_dot([
+                pos_translation_matrix,
+                rotation_matrix,
+                neg_translation_matrix
+            ])
+
+        object.move(movement_matrix)
+
     def get_movement_matrix(
             self,
-            current_point: np.ndarray,
-            target_point: np.ndarray,
+            current_point: np.ndarray = np.zeros(3),
+            target_point: np.ndarray = np.zeros(3),
             rotation_angle: float = 0,
             rotation_axis: str = None,
     ):
--- views/main/MainView.py
+++ views/main/MainView.py
@@ -77,6 +77,24 @@ class MainView(QMainWindow):
         return tab
 
     def actorControlsTab(self):
+        # Adds the radio buttons for the reference axis selection
+        ref_radio_group = QButtonGroup()
+        ref_radio_layout = QHBoxLayout()
+        ref_axes = ['actor', 'camera', 'world']
+        for index, axis in enumerate(ref_axes):
+            radio_label = '{} axis'.format(axis)
+            radio = QRadioButton(radio_label)
+            radio.value = axis
+            ref_radio_layout.addWidget(radio)
+            ref_radio_group.addButton(radio, index)
+
+        # Sets the world axis as the default rotation axis
+        ref_radio_group.button(2).setChecked(True)
+
+        # Saves the reference for the ref radio group
+        self.actor_controls['ref_radio_group'] = ref_radio_group
+
+        # Adds the sliders layout
         sliders_layout = QGridLayout()
         sliders_layout.setSpacing(10)
 
@@ -92,7 +110,24 @@ class MainView(QMainWindow):
             slider.valueChanged[int].connect(self.onActorControlsChange)
             sliders_layout.addWidget(slider, index, 1)
 
-        # Adds a knob for the actor rotation control
+        # Adds the radio buttons for the rotation axis selection
+        rot_radio_group = QButtonGroup()
+        rot_radio_layout = QHBoxLayout()
+        rot_axes = ['x', 'y', 'z']
+        for index, axis in enumerate(rot_axes):
+            radio_label = '{} axis'.format(axis)
+            radio = QRadioButton(radio_label)
+            radio.value = axis
+            rot_radio_layout.addWidget(radio)
+            rot_radio_group.addButton(radio, index)
+
+        # Sets the world axis as the default rotation axis
+        rot_radio_group.button(2).setChecked(True)
+
+        # Saves the reference for the rotation radio group
+        self.actor_controls['rot_radio_group'] = rot_radio_group
+
+        # Adds a dial for the actor rotation control
         dial_layout = QHBoxLayout()
         dial = QDial()
         dial.setRange(0, 360)
@@ -100,24 +135,11 @@ class MainView(QMainWindow):
         dial.valueChanged[int].connect(self.onActorControlsChange)
         dial_layout.addWidget(dial, alignment=Qt.AlignCenter)
 
-        # Adds the toggle buttons for the rotation axis selection
-        radio_layout = QGridLayout()
-        radio_layout.setSpacing(10)
-        for index, axis in enumerate(axes):
-            radio_label = '{} axis'.format(axis)
-            radio = QRadioButton(radio_label)
-            radio.value = axis
-            radio_layout.addWidget(radio, 0, index)
-            widget_key = '{}_radio'.format(axis)
-            self.actor_controls[widget_key] = radio
-
-        # Sets the z axis as the default rotation axis
-        self.actor_controls['z_radio'].setChecked(True)
-
         # Setup the tab layout
         layout = QVBoxLayout()
+        layout.addLayout(ref_radio_layout)
         layout.addLayout(sliders_layout)
-        layout.addLayout(radio_layout)
+        layout.addLayout(rot_radio_layout)
         layout.addLayout(dial_layout)
         layout.addStretch()
         tab = QWidget()
@@ -158,42 +180,34 @@ class MainView(QMainWindow):
         return tab
 
     def onActorControlsChange(self):
-        target_point = np.array([
+        target_coordinate = np.array([
             self.actor_controls['x_slider'].value(),
             self.actor_controls['y_slider'].value(),
             self.actor_controls['z_slider'].value()
         ])
 
-        rotation_axes = {
-            'x': self.actor_controls['x_radio'].isChecked(),
-            'y': self.actor_controls['y_radio'].isChecked(),
-            'z': self.actor_controls['z_radio'].isChecked()
-        }
-
-        rotation_axis = None
-        for (key, value) in rotation_axes.items():
-            if value:
-                rotation_axis = key
-                break
-
+        reference_axis = self.actor_controls['ref_radio_group'].checkedButton().value
+        rotation_axis = self.actor_controls['rot_radio_group'].checkedButton().value
         rotation_angle = self.actor_controls['rotation_dial'].value()
+
         self.controller.move_actor(
-            target_point=target_point,
+            target_coordinate=target_coordinate,
             rotation_angle=rotation_angle,
-            rotation_axis=rotation_axis
+            rotation_axis=rotation_axis,
+            reference_axis=reference_axis
         )
         self.controller.draw_world_components(plot_axis=self.world_chart.axis)
         self.world_chart.axis_equal_3D()
 
     def onCameraControlsChange(self):
-        target_point = np.array([
+        target_coordinate = np.array([
             self.camera_controls['x_slider'].value(),
             self.camera_controls['y_slider'].value(),
             self.camera_controls['z_slider'].value()
         ])
         rotation_angle = self.camera_controls['rotation_dial'].value()
         self.controller.move_camera(
-            target_point=target_point,
+            target_coordinate=target_coordinate,
             rotation_angle=rotation_angle,
             rotation_axis='z'
         )
